360d571e70afe236efdd593b8a5f38bb8b39498d
Leonardocabianca <leonardocabianca10@gmail.com>
2017-02-06 12:37:52 +0100

    Quiz Sport Versione 2 corretta

---
+++ Quiz_sport V 2/app/src/main/java/com/example/leonardo/quiz_sport/Activity3.java
@@ -0,0 +1,52 @@
+package com.example.leonardo.quiz_sport;
+
+
+import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.RadioButton;
+
+
+public class Activity3 extends AppCompatActivity {
+
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_3);
+
+            final RadioButton vero1 = (RadioButton) findViewById(R.id.rtrue1);
+            final RadioButton falso1 = (RadioButton) findViewById(R.id.rfalse1);
+            final RadioButton vero2 = (RadioButton) findViewById(R.id.rtrue2);
+            final RadioButton falso2 = (RadioButton) findViewById(R.id.rfalse2);
+            final RadioButton vero3 = (RadioButton) findViewById(R.id.rtrue3);
+            final RadioButton falso3 = (RadioButton) findViewById(R.id.rfalse3);
+            final RadioButton vero4 = (RadioButton) findViewById(R.id.rtrue4);
+            final RadioButton falso4 = (RadioButton) findViewById(R.id.rfalse4);
+
+            Button fine = (Button)findViewById(R.id.buttonExit1);
+            fine.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View view){
+                    if (vero1.isChecked() && vero2.isChecked() && falso3.isChecked() && vero4.isChecked()){
+                        Intent i = new Intent();
+                        i.putExtra("corretto", "2");
+                        Activity3.this.setResult(RESULT_OK, i);
+                        Activity3.this.finish();
+
+                    } else{
+                        Intent i = new Intent();
+                        i.putExtra("corretto","3");
+                        Activity3.this.setResult(RESULT_OK, i);
+                        Activity3.this.finish();
+                    }
+
+                }
+            });
+        }
+    }
+
+
+
